e9d5c93d7e00b05106a7cc28cb4177ebe1c23cd3
Stefan Blanke <stef@boognish.(none)>
2010-01-31 12:25:43 +0000

    Implemented Basic filenode browsing web interface. Applied JQUery UI interface.

--- soundandspace/views.py
+++ soundandspace/views.py
@@ -1,8 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-
+from soundandspace import models
 
 
 def index(request):
-	return render_to_response( "soundandspace/index.html", {}, context_instance=RequestContext(request) )
+	folders = models.WatchFolder.objects.filter()
+
+	return render_to_response( "soundandspace/index.html", {"folders":folders}, context_instance=RequestContext(request) )
+
+
+def view_node(request, node_id):
+	node = models.FileNode.objects.get(id=node_id)
+	return render_to_response( "soundandspace/view_node.html", {"node":node, "mobile":True}, context_instance=RequestContext(request) )
