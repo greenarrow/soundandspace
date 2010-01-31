5a4655bc3e231a82a78ec6b5a4d9b5238fa5e6d0
Stefan Blanke <stef@boognish.(none)>
2010-01-31 20:25:58 +0000

    General code tidy & comment

--- soundandspace/archive.py
+++ soundandspace/archive.py
@@ -3,12 +3,13 @@ from soundandspace import models
 import zipfile, uuid, os
 
 class ZipFile():
+	"""Object to create a package (zip file) for a given node"""
 	def __init__(self, node):
 		self.filename = os.path.join( settings.PACKAGE_PATH, str(uuid.uuid4() ) )
 		self.archive = zipfile.ZipFile(self.filename, 'w', zipfile.ZIP_DEFLATED)
 		
-		
 		for cn in node.get_child_nodes():
+			# Recursively add all nodes from the child node
 			self.add_node_tree(cn, cn)
 		
 		self.archive.close()
@@ -22,5 +23,4 @@ class ZipFile():
 		file_path = node.get_absolute_path()
 		zip_path = node.get_relative_path(target=root_node)
 		
-		print "fp", file_path, "zp", zip_path
 		self.archive.write(file_path, zip_path)
--- soundandspace/views.py
+++ soundandspace/views.py
@@ -3,28 +3,38 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.servers.basehttp import FileWrapper
 from django.conf import settings
+import mimetypes, os
 
 from soundandspace import models, archive
 
-import mimetypes, os
+
+
+def get_generic_values(request):
+	"""Returns dicionary of values used in all views"""
+	
+	# TODO: Useragent detection to switch to mobile browser mode
+	return {"mobile":True}
 
 
 def index(request):
+	"""The home screen"""
 	folders = models.WatchFolder.objects.filter()
-
-	return render_to_response( "soundandspace/index.html", {"folders":folders}, context_instance=RequestContext(request) )
+	return render_to_response( "soundandspace/index.html", get_generic_values().update( {"folders":folders} ), context_instance=RequestContext(request) )
 
 
 def view_node(request, node_id):
+	"""Display a given node in the db filesystem representation"""
 	node = models.FileNode.objects.get(id=node_id)
-	return render_to_response( "soundandspace/view_node.html", {"node":node, "mobile":True}, context_instance=RequestContext(request) )
+	return render_to_response( "soundandspace/view_node.html", get_generic_values().update( {"node":node}  ), context_instance=RequestContext(request) )
 
 
 def direct_download(self, node_id):
+	"""Returns data to download for a given node. Creates an archive if the node is a directory"""
+	
 	node = models.FileNode.objects.get(id=node_id)
 	
 	if node.directory and not node.is_empty():
-		# Build and archive
+		# We have directory with files in so build an archive
 		
 		zipfile = archive.ZipFile(node)
 		
@@ -37,11 +47,12 @@ def direct_download(self, node_id):
 		return response
 
 	elif node.directory:
-		# Empty directory
-		return HttpResponse("empty directory")
+		# We have an empty directory, we don't want that, that is rubbish
+		# TODO: Send the user something a bit more usefull
+		return HttpResponse("error: directory is empty")
 	
 	else:
-		# Just send the file as is
+		# The node is a file so just send it to the user as is.
 		filename = node.get_absolute_path()
 		wrapper = FileWrapper( file(filename) )
 		
